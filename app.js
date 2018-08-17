bd8c3b482162f86e3ec8ffcf212c03f04828e950
illusiongame01 <39041011+illusiongame01@users.noreply.github.com>
2018-08-17 10:47:22 +0700

    Update app.js

--- app.js
+++ app.js
@@ -53,12 +53,12 @@ app.post('/webhook', (req, res) => {
 
       // Check if the event is a message or postback and
       // pass the event to the appropriate handler function
-      if (webhook_event.message) {
-        handleMessage(sender_psid, webhook_event.message);        
-      } else if (webhook_event.postback) {
+     // if (webhook_event.message) {
+      //  handleMessage(sender_psid, webhook_event.message);        
+      //} else if (webhook_event.postback) {
         
         handlePostback(sender_psid, webhook_event.postback);
-      }
+     // }
       
     });
     // Return a '200 OK' response to all events
