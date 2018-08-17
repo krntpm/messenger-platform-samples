54ba0249370e249f9128a6aa93f8013d094fb584
illusiongame01 <39041011+illusiongame01@users.noreply.github.com>
2018-08-17 18:00:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -202,7 +202,7 @@ async function handleMessage(sender_psid, received_message) {
               });
                 console.log(JSON.stringify(resApi['data']['data']['message'][0]));
                 response = {
-                    "text": `${resApi['data']['data']['message'][0]}.`
+                    "text": `${resApi['data']['data']['message'][0].replace(/<[^>]+>/g, '')}.`
                 };
                 break;
         }
