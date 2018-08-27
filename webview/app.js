f6e85dbd6c903f213706ed5e6251794656051f38
illusiongame01 <39041011+illusiongame01@users.noreply.github.com>
2018-08-27 16:55:16 +0700

    Update app.js

--- webview/app.js
+++ webview/app.js
@@ -1,22 +1,4 @@
-/**
- * Copyright 2017-present, Facebook, Inc. All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
- *
- *
- * Starter Project for Messenger Platform Webview Tutorial
- *
- * Use this project as the starting point for following the
- * Messenger Platform webview tutorial.
- *
- * https://blog.messengerdevelopers.com/using-the-webview-to-create-richer-bot-to-user-interactions-ed8a789523c6
- *
- */
-
 'use strict';
-
-// Imports dependencies and set up http server
 const
     request = require('request'),
     express = require('express'),
