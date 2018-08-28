cbd44d283f94f752e6b24296e30f695293365c3b
illusiongame01 <39041011+illusiongame01@users.noreply.github.com>
2018-08-28 10:58:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -562,3 +562,56 @@ function call (path, payload, callback) {
     callback(body);
   });
 };
+
+
+// Accepts GET requests at the /webhook endpoint
+app.post('/passThreadControl', (req, res) => {
+   let body = req.body;
+    // Parse params from the webhook verification request
+    //let mode = req.query['hub.mode'];
+    //let token = req.query['hub.verify_token'];
+   // let challenge = req.query['hub.challenge'];
+
+    // Check if a token and mode were sent
+    /*if (mode && token) {
+
+        // Check the mode and token sent are correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Respond with 200 OK and challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }*/
+});
+
+
+app.post('/takeThreadControl', (req, res) => {
+   let body = req.body;
+   res.status(200).send(challenge);
+    // Parse params from the webhook verification request
+    //let mode = req.query['hub.mode'];
+    //let token = req.query['hub.verify_token'];
+    //let challenge = req.query['hub.challenge'];
+
+    // Check if a token and mode were sent
+   /* if (mode && token) {
+
+        // Check the mode and token sent are correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Respond with 200 OK and challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }*/
+});
+
