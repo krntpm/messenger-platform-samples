f777c75b7ad89b74738785879a5f0bcacdc1b3f5
illusiongame01 <39041011+illusiongame01@users.noreply.github.com>
2018-08-28 11:21:06 +0700

    Update app.js

--- app.js
+++ app.js
@@ -593,26 +593,27 @@ app.post('/passThreadControl', (req, res) => {
 
 app.post('/takeThreadControl', (req, res) => {
    let body = req.body;
-   res.status(200).send(body);
+  
     // Parse params from the webhook verification request
     //let mode = req.query['hub.mode'];
     //let token = req.query['hub.verify_token'];
     //let challenge = req.query['hub.challenge'];
 
     // Check if a token and mode were sent
-   /* if (mode && token) {
+    if (body.recipientId !== undefined && (body.term  === 'Exit' || body.term === 'End')) {
+        recipientToPrime(body.recipientId , body.term);
+        // Check the mode and token sent are correct      
+            console.log('WEBHOOK_BACK_TO_PRIME');
+            res.status(200).send(JSON.stringify('{STATUS:OK,MESSAGE:WEBHOOK_BACK_TO_PRIME}'));
 
-        // Check the mode and token sent are correct
-        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-
-            // Respond with 200 OK and challenge token from the request
-            console.log('WEBHOOK_VERIFIED');
-            res.status(200).send(challenge);
-
-        } else {
-            // Responds with '403 Forbidden' if verify tokens do not match
-            res.sendStatus(403);
-        }
-    }*/
+    }else {
+      res.sendStatus(403);
+    }
 });
+
+
+function recipientToPrime(userPsid , message){
+
+     HandoverProtocol.takeThreadControl(psid);
+}
 
