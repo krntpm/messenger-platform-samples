440aba8da2a3a5e14e5c6f83969f3f19c58048cd
illusiongame01 <39041011+illusiongame01@users.noreply.github.com>
2018-08-28 13:36:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -588,7 +588,7 @@ app.post('/ThreadControl', (req, res) => {
             res.status(200).send(JSON.stringify('{STATUS:OK,MESSAGE:WEBHOOK_BACK_TO_PRIME}'));
 
     } else if(body.recipientId !== undefined && body.term  !== undefined && body.method  === 'forback') {
-              callSendAPI(sender_psid, body.term)
+              callSendAPI(body.recipientId, body.term)
     } else {
       res.sendStatus(403);
     }
